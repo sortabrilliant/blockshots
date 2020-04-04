d4274d9e61617f368dd1e7335c9bd8d2fee06764
George Mamadashvili <georgemamadashvili@gmail.com>
2020-04-04 15:14:29 +0400

    Prepare release

--- blockshots.php
+++ blockshots.php
@@ -5,7 +5,7 @@
  * Description: The easiest way to take screenshots of your blocks.
  * Author:      sorta brilliant
  * Author URI:  https://sortabrilliant.com/
- * Version:     1.0.0
+ * Version:     1.0.1
  * License:     GPL-2.0-or-later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
